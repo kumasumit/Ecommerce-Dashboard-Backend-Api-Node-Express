563958f4710a97a5e0e91056786e63d108c9c35d
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-29 13:36:49 +0530

    login api basic done

--- routes/root.js
+++ routes/root.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 
 
 //1. Sign-Up Api 
-//A post to rgister/signup - create a new user in the database
+//An api with post request to rgister/signup - create a new user in the database
 router.post("/register", async (req, res)=>{
     let user = new User(req.body);
     let result = await user.save();
@@ -18,6 +18,36 @@ router.post("/register", async (req, res)=>{
     //after saving the created user we send back the created user as response without the password field
     res.send(result);
 })
-
+//2. Log-In Api 
+//An api with post request to login - create a new session for sign-up user in the database
+router.post("/login", async (req, res)=>{
+    //destructure email and password from req.body
+    const {email, password} = req.body;
+    //check if both fields are there, means both the fields exists, they are not empty
+    if(email && password)
+    {
+      let user = await User.findOne(req.body).select('-password');
+      if(user){
+        //if user is found
+        //send back the user without the password field
+        res.send(user);
+      }else{
+        //if no user is found corresponding to entered details
+        res.send({result: "Invalid User credentials"})
+      }
+    }else{
+        //if any field email and password is empty
+        res.send({result: "Please enter both the fields"})
+    }
+    let user = new User(req.body);
+    let result = await user.save();
+    //currently result is a json object, we need to convert to object data type
+    result.toObject();
+    //now result is of type object
+    //delete result.password will delete the password key value pair from result object, here result is the object and passoword is the key.
+    delete result.password;
+    //after saving the created user we send back the created user as response without the password field
+    res.send(result);
+})
 //finally we export the router at the bottom
 module.exports = router
